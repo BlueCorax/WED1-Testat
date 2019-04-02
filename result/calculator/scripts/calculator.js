de7d0788e278c22de870b8ff5d403d1e7231118e
flaviolev <6606236+flaviolev@users.noreply.github.com>
2019-04-02 13:41:08 +0200

    add implemenation folder with resources

---
+++ result/calculator/scripts/calculator.js
@@ -0,0 +1,27 @@
+/**
+ * core
+ */
+class Calculator {
+    constructor() {
+
+    }
+}
+
+
+/**
+ * UI
+ */
+window.addEventListener('DOMContentLoaded', function() {
+    const calculator = new Calculator();
+
+});
+
+
+/**
+ * Tests Scenarios
+ */
+const testCalc = new Calculator();
+console.log(""/*TODO*/, "should be", 17);
+console.log(""/*TODO*/, "should be", 15);
+console.log(""/*TODO*/, "should be", 30);
+console.log(""/*TODO*/, "should be", true); // true = hasError
